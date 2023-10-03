34e6f20ed0dc1be6bb9271677e2d4c56c438fd7a
TumenSan <sandanov.tumen.sib@mail.ru>
2023-10-04 00:48:14 +0700

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,15 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Header />
+        <div className="block rounded-lg bg-white p-6 shadow-lg dark:bg-neutral-700 dark:text-neutral-50">Hello world</div>
+        <div className="container w-80 mt-5 mx-auto bg-green-200 rounded-xl shadow">
+         <div className="text-3xl p-8 text-green-700 font-bold mb-5">
+           Welcome!
+           <p className="text-green-500 mt-3 text-lg">
+             I am learning Tailwind CSS
+           </p>
+         </div>
+       </div>
         <Routes>
           <Route path="/*" element={<>
             <Home />
